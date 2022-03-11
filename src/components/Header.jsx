9a05ab0ae0520f2163a3b280f33b3895788e1d30
cuot <deoret35@gmail.com>
2022-03-12 00:37:17 +0900

    head-radius

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,26 +3,24 @@ import React from "react";
 export default function Header() {
   return (
     <header
-      className="p-3 bg-opacity-75 text-white"
+      className="p-3 text-white"
       style={{ "background-color": "#055160" }}>
       <div className="container">
-        <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <div style={{ "text-align": "left" }}>
           <a
             href="/"
-            className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-            <svg
-              className="bi me-2"
-              width="40"
-              height="32"
-              role="img"
-              aria-label="Bootstrap">
-              {/* <use xlink:href="/"></use> */}
-            </svg>
+            className="align-items-center mb-2 mb-lg-0 text-decoration-none">
+            <div
+              className="fs-3 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0"
+              style={{
+                "background-color": "#fff",
+                borderRadius: "10px",
+                color: "black",
+                width: "100%",
+              }}>
+              &nbsp;&nbsp;로스트아크 분배 계산기
+            </div>
           </a>
-
-          <div className="fs-3 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-            로스트아크 분배 계산기
-          </div>
         </div>
       </div>
     </header>
